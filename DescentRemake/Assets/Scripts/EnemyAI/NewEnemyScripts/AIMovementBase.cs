870e3ab270e0c279afba5cb41877439322a7a13a
Gabriele Garofalo <gabrielegarofalo999@gmail.com>
2023-03-28 14:50:02 +0200

    added new AI scripts

---
+++ DescentRemake/Assets/Scripts/EnemyAI/NewEnemyScripts/AIMovementBase.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AIMovementBase : MonoBehaviour
+{
+    public Transform m_Player;
+
+    public float m_DetectionDistance, m_OuterCombatDistance, m_InnerCombatDistance;
+    public float m_Speed, m_AngularSpeed;
+    [Tooltip("the minimum time after which the enemy can change direction")]
+    public float m_ChangeDirectionTimer;
+    public bool m_PlayerInSight { get; set; }
+
+    protected float m_distanceFromPlayer;
+    public float m_changeDirectionTimer;
+
+    protected enum MovementState { idle, detecting, combat, repositioning };
+    protected MovementState m_movementState;
+    protected MovementState m_lastMovementState;
+
+    protected delegate void MovementBehaviour();
+    protected MovementBehaviour m_movementBehaviour;
+
+    protected void Awake()
+    {
+        m_changeDirectionTimer = m_ChangeDirectionTimer;
+        m_movementBehaviour = IdleBehaviour;
+    }
+
+    protected void Update()
+    {
+        UpdateMovementState();
+        UpdateMovementBehaviour();
+        m_movementBehaviour();
+    }
+
+
+
+    /// <summary>
+    /// Updates the state of the movement based on the distance from player
+    /// </summary>
+    protected void UpdateMovementState()
+    {
+        m_distanceFromPlayer = Vector3.Distance(transform.position, m_Player.position);
+
+        if (m_distanceFromPlayer > m_DetectionDistance) m_movementState = MovementState.idle;
+        else if (m_OuterCombatDistance < m_distanceFromPlayer && m_distanceFromPlayer <= m_DetectionDistance) m_movementState = MovementState.detecting;
+        else if (m_InnerCombatDistance <= m_distanceFromPlayer && m_distanceFromPlayer <= m_OuterCombatDistance) m_movementState = MovementState.combat;
+        else if (m_distanceFromPlayer < m_InnerCombatDistance) m_movementState = MovementState.repositioning;
+    }
+
+    /// <summary>
+    /// Updates the the movement behaviour based on the movement state
+    /// </summary>
+    protected void UpdateMovementBehaviour()
+    {
+        if (m_movementState != m_lastMovementState)
+        {
+            switch (m_movementState)
+            {
+                case MovementState.idle:
+                    m_movementBehaviour = IdleBehaviour;
+                    break;
+                case MovementState.detecting:
+                    m_movementBehaviour = DetectingBehaviour;
+                    break;
+                case MovementState.combat:
+                    m_movementBehaviour = CombatBehaviour;
+                    break;
+                case MovementState.repositioning:
+                    m_movementBehaviour = RepositioningBehaviour;
+                    break;
+            }
+
+            m_lastMovementState = m_movementState;
+        }
+    }
+
+
+    protected virtual void IdleBehaviour()
+    {
+        Debug.Log("idle behaviour");
+    }
+
+    /// <summary>
+    /// Makes the enemy go away from player to recover the combat distance
+    /// </summary>
+    protected virtual void DetectingBehaviour()
+    {
+        Debug.Log("detecting behaviour");
+    }
+
+    /// <summary>
+    /// Makes the enemy follow a specific movement pattern around the player
+    /// </summary>
+    protected abstract void CombatBehaviour();
+
+    /// <summary>
+    /// Makes the enemy returns to target combat distance from player
+    /// </summary>
+    protected virtual void RepositioningBehaviour()
+    {
+        Debug.Log("repositioning behaviour");
+    }
+}
---
+++ DescentRemake/Assets/Scripts/EnemyAI/NewEnemyScripts/AITypeOne.cs
@@ -0,0 +1,205 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AITypeOne : AIMovementBase
+{
+    public Vector3 m_randomAxis = Vector3.up;
+
+    protected override void DetectingBehaviour()
+    {
+        base.DetectingBehaviour();
+
+        Vector3 direction = m_Player.position - transform.position;
+        RaycastHit hitInfo;
+        Physics.Raycast(transform.position, direction, out hitInfo, m_DetectionDistance);
+
+        if (hitInfo.collider != null && hitInfo.collider.gameObject.transform == m_Player.transform)
+        {
+            //Debug.Log("player in sight");
+
+            transform.position = Vector3.MoveTowards(transform.position, m_Player.position, m_Speed * Time.deltaTime);
+
+            //Look at player - start
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, m_AngularSpeed * Time.deltaTime);
+            //Look at player - end
+        }
+    }
+
+    protected override void CombatBehaviour()
+    {
+        Debug.Log("combat behaviour");
+
+        //Look at player - start
+        Vector3 direction = m_Player.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, m_AngularSpeed * Time.deltaTime);
+        //Look at player - end
+
+        CombatMovementPattern();
+    }
+
+
+    protected override void RepositioningBehaviour()
+    {
+        base.RepositioningBehaviour();
+        
+        //Look at player - start
+        Vector3 direction = m_Player.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, m_AngularSpeed * Time.deltaTime);
+        //Look at player - end
+
+        transform.position = Vector3.MoveTowards(transform.position, m_Player.position, -m_Speed * Time.deltaTime); //goes away from player 
+    }
+
+    /// <summary>
+    /// The movement pattern of this type of enemy
+    /// </summary>
+    private void CombatMovementPattern()
+    {
+        //Rotate around player randomly - start
+        if (ChangeDirectionCondition())
+        {
+            Debug.Log("ChangeDirection");
+            m_randomAxis = RandomWorldAxis() * RandomSign();
+        }
+        else transform.RotateAround(m_Player.position, m_randomAxis.normalized, 20f * Time.deltaTime);
+        //Rotate around player randomly - end
+
+
+        /// <summary>
+        /// NOTE: not the correct model written on GDD but similar and better performing
+        /// </summary>
+        /// 
+        //if (m_changeDirectionTimer > 0f)
+        //{
+        //    m_changeDirectionTimer -= Time.deltaTime;
+        //    transform.RotateAround(m_Player.position, m_randomAxis.normalized, 20f * Time.deltaTime);
+        //}
+        //else
+        //{
+        //    m_changeDirectionTimer = m_ChangeDirectionTimer;
+        //    m_randomAxis = new Vector3(Random.value, Random.value, Random.value);
+        //}
+    }
+
+
+    /// <summary>
+    /// Returns a random world axis based on enemy position
+    /// </summary>
+    /// <returns>Random world axis</returns>
+    private Vector3 RandomWorldAxis()
+    {
+        if (Mathf.Abs(transform.position.z - m_Player.position.z) >= m_InnerCombatDistance) //if the enemy is on the z axis of the world
+        {
+            //can return only x and y axis
+            if (m_randomAxis.y != 0) return Vector3.right;
+            else return Vector3.up;
+        }
+        if (Mathf.Abs(transform.position.x - m_Player.position.x) >= m_InnerCombatDistance) //if the enemy is on the x axis of the world
+        {
+            //can return only z and y axis
+            if (m_randomAxis.y != 0) return Vector3.forward;
+            else return Vector3.up;
+        }
+        if (Mathf.Abs(transform.position.y - m_Player.position.y) >= m_InnerCombatDistance) //if the enemy is on the y axis of the world
+        {
+            //can return only x and z axis
+            if (m_randomAxis.x != 0) return Vector3.forward;
+            else return Vector3.right;
+        }
+
+        return Vector3.up;
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <returns>true if the enemy is in a legal positions to change movement direction</returns>
+    private bool ChangeDirectionCondition()
+    {
+
+        if(m_changeDirectionTimer > 0)
+        {
+            m_changeDirectionTimer -= Time.deltaTime;
+            return false;
+        }
+
+        if (m_randomAxis.y != 0) //if the rotating axis is Y
+        {
+            //change direction only if the enemy is on the Z or X axis
+            if(Mathf.Abs(transform.position.x - m_Player.position.x) < 0.3f || Mathf.Abs(transform.position.z - m_Player.position.z) < 0.3f)
+            {
+                m_changeDirectionTimer = m_ChangeDirectionTimer;
+                return true;
+            }
+        }
+        if (m_randomAxis.x != 0) //if the rotating axis is X
+        {
+            //change direction only if the enemy is on the Z or Y axis
+            if (Mathf.Abs(transform.position.y - m_Player.position.y) < 0.3f || Mathf.Abs(transform.position.z - m_Player.position.z) < 0.3f)
+            {
+                m_changeDirectionTimer = m_ChangeDirectionTimer;
+                return true;
+            }
+        }
+        if (m_randomAxis.z != 0) //if the rotating axis is Z
+        {
+            //change direction only if the enemy is on the Y or X axis
+            if (Mathf.Abs(transform.position.x - m_Player.position.x) < 0.3f || Mathf.Abs(transform.position.y - m_Player.position.y) < 0.3f)
+            {
+                m_changeDirectionTimer = m_ChangeDirectionTimer;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <returns>return randomly 1 or -1</returns>
+    private int RandomSign()
+    {
+        return Random.value < .5 ? 1 : -1;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ///// <summary>
+    ///// makes the enemy moves towards the player
+    ///// </summary>
+    //private void MoveTowardsPlayer()
+    //{
+    //    transform.position = Vector3.MoveTowards(transform.position, m_Player.position, m_Speed * Time.deltaTime);
+    //}
+
+    ///// <summary>
+    ///// makes the enemy look at the player
+    ///// </summary>
+    ///// <param name="direction"></param>
+    //private void LookAtPlayer(Vector3 direction)
+    //{
+    //    Quaternion targetRotation = Quaternion.LookRotation(direction);
+    //    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, m_AngularSpeed * Time.deltaTime);
+    //}
+}
